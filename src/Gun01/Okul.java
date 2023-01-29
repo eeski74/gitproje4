eb8f17504c6aa6ecae0d422bdbe9679351d9bf2d
eeski74 <123576850+eeski74@users.noreply.github.com>
2023-01-29 11:24:28 +0300

    üc ve dört task yapıldı.

--- src/Gun01/Okul.java
+++ src/Gun01/Okul.java
@@ -3,5 +3,6 @@ package Gun01;
 public class Okul {
     public static void main(String[] args) {
         System.out.println("Task 3 yapıldı");
+        System.out.println("Task 4 yapıldı");
     }
 }
